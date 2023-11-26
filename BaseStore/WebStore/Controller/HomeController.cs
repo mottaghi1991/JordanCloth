0e91e60b2ee5c615f53a06af2fe337464b14e00e
motaghi1991 <ali.mottaghi1991@gmail.com>
2023-11-26 16:17:34 +0330

    ADD Theme

---
+++ BaseStore/WebStore/Controller/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controller
+{
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- BaseStore/WebStore/Startup.cs
+++ BaseStore/WebStore/Startup.cs
@@ -51,6 +51,15 @@ namespace WebStore
             {
                 endpoints.MapRazorPages();
             });
+            app.UseEndpoints(endpoints =>
+            {
+             
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            });
         }
     }
 }
